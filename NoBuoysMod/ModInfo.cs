257514dd1a1af1802e43e42b649c59be19280c23
vonLeebpl <vonleebpl@gmail.com>
2017-06-15 23:36:58 +0200

    Commit options

---
+++ NoBuoysMod/ModInfo.cs
@@ -0,0 +1,29 @@
+﻿using ICities;
+using UnityEngine;
+using NoBuoysMod.TranslationFramework;
+
+namespace NoBuoysMod
+{
+    public class ModInfo : IUserMod
+    {
+        Translation translation = new Translation();
+
+        public string Name => "Buoys Remover";
+
+        public string Description => translation.GetTranslation( "NBM_DESCRIPTION" );
+
+
+        public void OnSettingsUI( UIHelperBase helper )
+        {
+            NoBuoysModConfig config = Configuration<NoBuoysModConfig>.Load();
+
+            bool isEnabled = config.NoBuoysEnabled;
+
+            helper.AddCheckbox( "Remove buoys", isEnabled, sel => {
+                config.NoBuoysEnabled = sel;
+                Configuration<NoBuoysModConfig>.Save();
+            } );
+        }
+
+    }
+}
